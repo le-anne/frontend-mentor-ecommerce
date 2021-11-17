1145997d146df825eb7bf4adfe801143ddc8903c
Leanne <leannevictoriaharris@gmail.com>
2021-11-16 16:31:00 -0800

    ecommerce project

---
+++ src/components/Main/index.js
@@ -0,0 +1,13 @@
+import  React from 'react';
+import CartController from '../CartController';
+import Description from '../Description';
+import { Container } from './styles';
+
+const Main = () => (
+  <Container>
+    <Description />
+    <CartController />
+  </Container>
+);
+
+export default Main;
---
+++ src/pages/ProductPage/index.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import { PropTypes } from 'prop-types';
+import { Container } from './styles';
+
+const ProductPage = ({ children }) => (
+  <Container>
+    {children}
+  </Container>
+);
+
+export default ProductPage;
+
+ProductPage.propTypes = {
+  children: PropTypes.node.isRequired,
+};
